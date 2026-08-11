18fad35ea1ab913196a22247f2eb40c8e7d27b9c
Iris <i.dorland-mooiweer@zuiderzeeland.nl>
2026-08-11 10:34:52 +0200

    naar hoe de opdracht eigenlijk moest

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,35 +1,22 @@
-prijzen = {
-    "aardbei": 3,
-    "vanille": 4,
-    "chocolade": 5
-}
-aanbieding = 0.8 * prijzen["aardbei"]
-#print(aanbieding)
-reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts €{aanbieding:.2f}"
-#print(reclame_tekst)
-reclame_tekst2 = reclame_tekst[:62]
-#print(reclame_tekst2)
-reclame_tekst3 = reclame_tekst2.upper()
-#print(reclame_tekst3)
+def print_aanbieding():
+    prijzen = {
+        "aardbei": 3,
+        "vanille": 4,
+        "chocolade": 5
+    }
+    aanbieding = 0.8 * prijzen["aardbei"]
 
-reclame_tekst4 = reclame_tekst3.split(" ")
-#print(reclame_tekst4)
+    reclame_tekst = f"Vandaag in de aanbiedinng: vanille-ijs, 1 liter - slechts €{aanbieding}"
+    reclame_tekst2 = reclame_tekst[:63]
+    reclame_tekst3 = reclame_tekst2.upper()
+    reclame_tekst4 = reclame_tekst3.split(" ")
 
-#Opdracht8
-#el = ""
-# for item in reclame_tekst4:
-#    el += f"{item}" + "\n"
-#print(el)
+    el = ""
+    for item in reclame_tekst4:
+        el =f"{item}"
+        if  len(el)<5:
+            print(el.lower())
+        else:
+            print(el)
 
-#opdracht 9
-#el= el.lower()
-#print(el)
-
-#opdracht10
-el = ""
-for item in reclame_tekst4:
-    el =f"{item}"
-    if  len(el)<5:
-        print(el.lower())
-    else:
-        print(el)
+print_aanbieding()
